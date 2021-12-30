d75c935382771cc2dbeaf13540ce277f7d6ffc2c
falpi <falpi@stud.hs-offenburg.de>
2021-12-30 20:41:07 +0100

    Added .sol file

---
+++ Variable.sol
@@ -0,0 +1,19 @@
+
+var a = 1;
+var b = "Hello";
+var c = false;
+var d = 2.5;
+// Java is an untyped Language ( do not need to specify the type)
+// Solidity IS a TYPED Language
+
+int a = 1; // can be negative
+uint aa = 2; // unsigned integer, can't be negative
+string b = "hello";
+bool c = false;
+// decimal number do not exist in solidity
+address d = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
+
+
+
+
